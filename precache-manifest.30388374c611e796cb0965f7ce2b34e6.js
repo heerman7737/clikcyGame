bd7020d53d2b5f8cd2e41901dab3883d66e90a4f
Giang <giang.to.7737@gmail.com>
2019-06-25 18:06:01 -0700

    Updates

---
+++ precache-manifest.30388374c611e796cb0965f7ce2b34e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe7b926fb5e47c8984af8cf98e68e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168793b04911c531647b",
+    "url": "/static/css/main.b7b8fde2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "168793b04911c531647b",
+    "url": "/static/js/main.dc30753f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/Banner/Banner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./Banner.css"
-
-const Banner = props => (
-  <article>
-    <p>{props.status} | Score: {props.currentScore} | Top Score: {props.topScore}</p>
-  </article>
-);
-export default Banner;
